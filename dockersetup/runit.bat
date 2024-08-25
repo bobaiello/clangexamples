2c00bf7d8bf82f4bbea867cf9fbfc4f850edfbb9
Bob Aiello <raiello1@northwell.edu>
2024-08-25 18:38:26 -0400

    dockersetup

---
+++ dockersetup/runit.bat
@@ -0,0 +1,14 @@
+#docker build -t ansibleworkbench .
+
+#docker run -p 8080:8080 -d ansibleworkbench
+
+#docker exec -it ansibleworkbench /bin/bash
+
+#docker run -ti -p 8080:8080 ansibleworkbench /bin/bash
+
+docker run -ti ansibleworkbench /bin/bash
+
+
+#ssh raiello1@10.246.69.70
+
+#docker run -it ubuntu /bin/bash
