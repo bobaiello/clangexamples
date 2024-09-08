04fb5d4aec050830dac59da83b21801ea13b1bed
Bob Aiello <bob.aiello@ieee.org>
2024-09-08 22:24:43 +0000

    update util

---
+++ utilexample/program.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <util.h>
+
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("\nHello, World!\n\n\n");
+   return 0;
+}
+
---
+++ utilexample/util.c
@@ -0,0 +1,26 @@
+#include <util.h>
+
+
+adder (x, y)
+{
+
+}
+
+
+subtract (x,y)
+
+{
+
+}
+
+times (x,y)
+{
+
+}
+
+
+divide (x,y)
+{
+
+}
+
