3a0a665a3439863306fd23f7b1967e06477b6c32
Bob Aiello <bob.aiello@ieee.org>
2024-09-08 22:29:01 +0000

    update program

---
+++ programexample/program.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <util.h>
+
+
+adder (x,y)
+{
+
+}
+
+minus (x,y)
+{
+
+}
+
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("\nHello, World!\n\n\n");
+   adder(2,9)
+   minus(10,3)
+
+   return 0;
+}
+
+
